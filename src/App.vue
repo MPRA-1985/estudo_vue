5cb845f606fc9484ebd0b1a571e7360e20f0c911
MPRAndrade <marceloandrade.design@gmail.com>
2023-05-14 23:01:35 -0300

    Ajustes de estilo

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
   import MenuComponent from '@/components/MenuComponent.vue'
 
   export default {
+    
     components: { MenuComponent },
 
     created() {
@@ -22,7 +23,7 @@
 
 <style>  
 
-  body {
+  #app {
     font-size: 16px;
     font-family: Poppins;
     font-style: normal;
@@ -31,9 +32,44 @@
     line-height: inherit;
   }
 
-  section.blue-section {
+  #app section.blue-section {
     background-color: #02538f;
     color: #fff;
+  }
+
+  #app .curta {
+      margin-top: 3em;
+  }
+
+  #app h1 {
+      margin-bottom: 1.7em;
+      font-weight: 600;
+      font-size: 2.5em;
+  }
+
+  #app h2 {
+      font-family: Poppins;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 35px;
+      line-height: 50px;
+      text-align: center;
+      color: #fff;
+  }
+
+  #app h3 {
+      font-family: Poppins;
+      font-style: normal;
+      font-weight: 700;
+      font-size: .85em;
+      align-items: center;
+      text-align: center;
+      text-transform: uppercase;
+      margin-bottom: 20px
+  }
+
+  #app hr {
+      opacity: .2;
   }
   
 
