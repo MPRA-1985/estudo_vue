c5ffa105e15c9c97ed6aa75bea580d12eae9610c
MPRAndrade <marceloandrade.design@gmail.com>
2023-05-15 17:50:48 -0300

    Filtro de cursos por área

--- src/store/index.js
+++ src/store/index.js
@@ -37,7 +37,10 @@ export default createStore({
         const response = await fetch(url);
         const jsonp = await response.text(); // Converte o JSONP para string
         const jsonString = jsonp.substring( jsonp.indexOf( '(') + 1, jsonp.lastIndexOf(')' ) ); // usa o método "substring" para remover o preenchimento JSONP da string
-        this.state.listaCursos = JSON.parse(jsonString); // converte a string tratada para JSON
+        let listaCursos = JSON.parse(jsonString); // converte a string tratada para JSON
+
+
+        this.state.listaCursos = listaCursos.result.cursos;
 
         console.log(this.state.listaCursos);
 
@@ -51,21 +54,35 @@ export default createStore({
 
     listaCursosPorArea (context) {
 
-      // continuar daqui para organizar os cursos por área
+      // estudar o código abaixo
 
-      var arrCursosPorArea = [];
+      const dados = this.state.listaCursos;
+      
+      const resultado = [];
+      
+      // Percorre cada chave do objeto original
+      for ( const chave in dados ) {
 
-      for ( var curso in this.state.listaCursos.result.cursos ) {
-
-        if ( !arrCursosPorArea.includes(this.state.listaCursos.result.cursos[curso].area) ) {
-
-          arrCursosPorArea.push( this.state.listaCursos.result.cursos[curso].area = {} );
-
+        const curso = dados[chave];
+        const area = curso.area;
+      
+        // Verifica se já existe um objeto para a área atual no resultado
+        const areaExistente = resultado.find(objeto => Object.keys(objeto)[0] === area);
+      
+        // Cria um novo objeto para o curso atual
+        const novoCurso = { [chave]: curso };
+      
+        // Se já existe um objeto para a área, adiciona o novo curso a ele
+        if (areaExistente) {
+          areaExistente[area].push(novoCurso);
+        } else {
+          // Caso contrário, cria um novo objeto para a área e adiciona o curso
+          resultado.push({ [area]: [novoCurso] });
         }
 
       }
-
-      console.log( arrCursosPorArea )
+      
+      console.log(resultado);
 
       context.commit('isLoaded');
 
